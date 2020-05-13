5a64ff56689fca5ad5f6f233878c515a6f0d3f73
Julio522 <46690617+Julio522@users.noreply.github.com>
2020-05-13 16:36:59 -0400

    front end + node project

--- h.js
+++ h.js
@@ -1,8 +1,17 @@
-const http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
+var http = require("http"),
+	//socketio = require("socket.io"),
+	fs = require("fs");
+
+// Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
+var app = http.createServer(function(req, resp){
+	// This callback runs when a new connection is made to our HTTP server.
+	
+	fs.readFile("fluff.html", function(err, data){
+		// This callback runs when the client.html file has been read from the filesystem.
+		
+		if(err) return resp.writeHead(500);
+		resp.writeHead(200);
+		resp.end(data);
 	});
-	res.end('Hello World\n');
-}).listen(3456);
-console.log('Server running at http://localhost:3456/');
+});
+app.listen(3456);
